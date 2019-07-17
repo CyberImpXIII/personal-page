a2198913de8d1faca0e9955c3738b6f8d7c865f5
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-07-17 10:13:32 -0400

    layout additions

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -3,6 +3,12 @@ import React from "react";
 export default Sidebar = props => {
   return(
     <>
+      <div id='Project-one'></div>
+      <div id='Project-two'></div>
+      <div id='Project-three'></div>
+      <div id='Project-four'></div>
+      <div id='Project-five'></div>
+      <div id='Project-six'></div>
         This is where the Sidebar will go.
     </>
   );
--- client/components/Topbar.js
+++ client/components/Topbar.js
@@ -3,6 +3,7 @@ import React from "react";
 export default Topbar = props => {
   return(
     <>
+        <div id='Home'>Home</div><div id='contact'>Contact</div>
         This is where the Topbar go.
     </>
   );
