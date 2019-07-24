8717468d034a3a3e3e662f29cea536360aef94ca
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-07-24 16:00:04 -0400

    project placholders

--- client/app.js
+++ client/app.js
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = props => {
+  const [ctx, setCtx] = useState({});
+  useEffect(()=>{
+    var canvas = document.getElementById('sphere3d');              
+    if(canvas.getContext){
+      // 2D Context found
+      var ctx = canvas.getContext('2d');
+      
+    } else {
+      // 2D Context not found
+    }
+  },[])
   return (
     <>
     <Topbar />
--- client/components/CanvasFocus.js
+++ client/components/CanvasFocus.js
@@ -3,7 +3,11 @@ import React from "react";
 export default CanvasFocus = props => {
   return(
     <>
-        This is where the interactive canvas element will go.
+    <canvas id="sphere3d" width="100" height="100">
+    Text to alert visitors that the Canvas
+    is not supported by your browser. 
+    </canvas>
+
     </>
   );
 };
